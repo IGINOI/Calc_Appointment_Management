5e2b5ddd4f8ea659d5497226eb57ba64075dd53b
Davide <davidefacchinelli2003@gmail.com>
2024-11-15 23:42:25 +0100

    added the automatic date computation and file access

--- wa_sender_ods.py
+++ wa_sender_ods.py
@@ -1,19 +1,7 @@
 import pandas as pd
 
 # Read the file for contact list and for appointment list
-contact_list = pd.read_excel('Centro_Fit_Calendar_2025.ods', engine='odf', sheet_name='Contatti', header=1)
-appointments_list = pd.read_excel('Centro_Fit_Calendar_2025.ods', engine='odf', sheet_name='Gennaio', header=1)
-february_appointments_list = pd.read_excel('Centro_Fit_Calendar_2025.ods', engine='odf', sheet_name='Febbraio', header=1)
-march_appointments_list = pd.read_excel('Centro_Fit_Calendar_2025.ods', engine='odf', sheet_name='Marzo', header=1)
-april_appointments_list = pd.read_excel('Centro_Fit_Calendar_2025.ods', engine='odf', sheet_name='Aprile', header=1)
-may_appointments_list = pd.read_excel('Centro_Fit_Calendar_2025.ods', engine='odf', sheet_name='Maggio', header=1)
-june_appointments_list = pd.read_excel('Centro_Fit_Calendar_2025.ods', engine='odf', sheet_name='Giugno', header=1)
-july_appointments_list = pd.read_excel('Centro_Fit_Calendar_2025.ods', engine='odf', sheet_name='Luglio', header=1)
-august_appointments_list = pd.read_excel('Centro_Fit_Calendar_2025.ods', engine='odf', sheet_name='Agosto', header=1)
-september_appointments_list = pd.read_excel('Centro_Fit_Calendar_2025.ods', engine='odf', sheet_name='Settembre', header=1)
-october_appointments_list = pd.read_excel('Centro_Fit_Calendar_2025.ods', engine='odf', sheet_name='Ottobre', header=1)
-november_appointments_list = pd.read_excel('Centro_Fit_Calendar_2025.ods', engine='odf', sheet_name='Novembre', header=1)
-december_appointments_list = pd.read_excel('Centro_Fit_Calendar_2025.ods', engine='odf', sheet_name='Dicembre', header=1)
+contact_list = pd.read_excel('Appointment_Schedule_2025.ods', engine='odf', sheet_name='Contatti', header=1)
 
 #Add a column with the full name of each contact
 contact_list['Full Name'] = (contact_list['Cognome'] + ' ' + contact_list['Nome']).str.strip()
@@ -22,12 +10,40 @@ days_list = ['Lunedì', 'Martedì', 'Mercoledì', 'Giovedì', 'Venerdì', 'Sabat
 
 # Extract the email list for 7 days
 for i in range(7):
-    # Create the format of the day
-    formatted_day = f"{days_list[i]} {i+6}"    ##!! FOR NOW THE DAYS ARE FIXED (the calendar start in january we cannot read the today date)
-    print("The day " + formatted_day + " the email list is:")
+    day_of_today = days_list[(pd.Timestamp.today().date().isoweekday() - 1 + i) % 7] + ' ' + str((pd.Timestamp.today().date() + pd.Timedelta(days=i)).day)
+    print(day_of_today)
+    match pd.Timestamp.today().date().month:
+        case "1":
+            appointments_list = pd.read_excel('Appointment_Schedule_2025.ods', engine='odf', sheet_name='Gennaio', header=1)
+        case "2":
+            appointments_list = pd.read_excel('Appointment_Schedule_2025.ods', engine='odf', sheet_name='Febbraio', header=1)
+        case "3":
+            appointments_list = pd.read_excel('Appointment_Schedule_2025.ods', engine='odf', sheet_name='Marzo', header=1)
+        case "4":
+            appointments_list = pd.read_excel('Appointment_Schedule_2025.ods', engine='odf', sheet_name='Aprile', header=1)
+        case "5":
+            appointments_list = pd.read_excel('Appointment_Schedule_2025.ods', engine='odf', sheet_name='Maggio', header=1)
+        case "6":
+            appointments_list = pd.read_excel('Appointment_Schedule_2025.ods', engine='odf', sheet_name='Giugno', header=1)
+        case "7":
+            appointments_list = pd.read_excel('Appointment_Schedule_2025.ods', engine='odf', sheet_name='Luglio', header=1)
+        case "8":
+            appointments_list = pd.read_excel('Appointment_Schedule_2025.ods', engine='odf', sheet_name='Agosto', header=1)
+        case "9":
+            appointments_list = pd.read_excel('Appointment_Schedule_2025.ods', engine='odf', sheet_name='Settembre', header=1)
+        case "10":
+            appointments_list = pd.read_excel('Appointment_Schedule_2025.ods', engine='odf', sheet_name='Ottobre', header=1)
+        case "11":
+            appointments_list = pd.read_excel('Appointment_Schedule_2025.ods', engine='odf', sheet_name='Novembre', header=1)
+        case "12":
+            appointments_list = pd.read_excel('Appointment_Schedule_2025.ods', engine='odf', sheet_name='Dicembre', header=1)
+        case _:
+            appointments_list = pd.read_excel('Appointment_Schedule_2025.ods', engine='odf', sheet_name='Gennaio', header=1)
+
+    
     
     # Extract the appointments for the day i
-    appointments_day_i = appointments_list[appointments_list['Giorno'] == formatted_day]
+    appointments_day_i = appointments_list[appointments_list['Giorno'] == day_of_today]
 
     #################################
     ##### First slot 09:30-11:00 ####
@@ -45,7 +61,7 @@ for i in range(7):
     for customer in names_day_i_first_slot:
         for contact in  list(contact_list['Full Name']):
             if customer == contact:
-                email_list_first_slot.append(contact_list[contact_list['Full Name'] == contact]['E-mail'].values[0])
+                email_list_first_slot.append(str(contact_list[contact_list['Full Name'] == contact]['Numero Cellulare'].values[0]))
 
 
     ###################################
@@ -64,7 +80,7 @@ for i in range(7):
     for customer in names_day_i_second_slot:
         for contact in  list(contact_list['Full Name']):
             if customer == contact:
-                email_list_second_slot.append(contact_list[contact_list['Full Name'] == contact]['E-mail'].values[0])
+                email_list_second_slot.append(str(contact_list[contact_list['Full Name'] == contact]['Numero Cellulare'].values[0]))
 
 
     ###################################
@@ -83,7 +99,7 @@ for i in range(7):
     for customer in names_day_i_third_slot:
         for contact in  list(contact_list['Full Name']):
             if customer == contact:
-                email_list_third_slot.append(contact_list[contact_list['Full Name'] == contact]['E-mail'].values[0])
+                email_list_third_slot.append(str(contact_list[contact_list['Full Name'] == contact]['Numero Cellulare'].values[0]))
 
 
     ###################################
@@ -102,7 +118,7 @@ for i in range(7):
     for customer in names_day_i_fourth_slot:
         for contact in  list(contact_list['Full Name']):
             if customer == contact:
-                email_list_fourth_slot.append(contact_list[contact_list['Full Name'] == contact]['E-mail'].values[0])
+                email_list_fourth_slot.append(str(contact_list[contact_list['Full Name'] == contact]['Numero Cellulare'].values[0]))
 
     # Print the resulting email list
     print(email_list_first_slot)
