b7bc4969b0b724c095574b10e13976fbddf345ab
Davide <davidefacchinelli2003@gmail.com>
2024-11-18 00:20:01 +0100

    modified the time slots and cleaned the code

--- wa_sender_ods.py
+++ wa_sender_ods.py
@@ -1,56 +1,46 @@
 import pandas as pd
 import pywhatkit as kit
 
-days_list = ['Lunedì', 'Martedì', 'Mercoledì', 'Giovedì', 'Venerdì', 'Sabato', 'Domenica']
+#FUNCTION DEFINITION -> this function is used to send messagges with the pywhatkit library
+def send_message(phone_number_list: list, appointment_day: str, appointment_time_slot: str):
+    wa_message = 'Ciao, ti ricordiamo il tuo appuntamento del ' + appointment_day + ' alle ore ' + appointment_time_slot + '. A presto! CentroFit'
+    for number in phone_number_list:
+        kit.sendwhatmsg_instantly('+'+str(number), wa_message, 4, True)
 
-# Extract the email list for 7 days
+
+
+
+months_list = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre']
+days_list = ['Lunedì', 'Martedì', 'Mercoledì', 'Giovedì', 'Venerdì', 'Sabato', 'Domenica']
+time_slots_list = ['8:30-10:00', '10:30-12:00', '12:00-13:30', '14:30-16:00', '16:00-17:30', '17:30-19:00']
+
 for i in range(7):
-    # Read the file of the right year
-    year = str(pd.Timestamp.today().date().year)
-    contact_list = pd.read_excel('Appointment_Schedule_'+str(pd.Timestamp.today().date().year)+'.ods', engine='odf', sheet_name='Contatti', header=1)
+    # Find: year - month - month_day - week_day - appointment_day(in the wanted format)
+    year = (pd.Timestamp.today().date() + pd.Timedelta(days=i)).year
+    month = months_list[(pd.Timestamp.today().date() + pd.Timedelta(days=i)).month -1]
+    month_day = (pd.Timestamp.today().date() + pd.Timedelta(days=i)).day
+    week_day = days_list[(pd.Timestamp.today().date().isoweekday() - 1 + i) % 7]
+    appointment_day = week_day + ' ' + str(month_day)
+    print(appointment_day)
     
-    # Add a column to the file in order to have the full name of the contact
+    #Reading the contact list and adding a new column with the full name
+    contact_list = pd.read_excel('Appointment_Schedule_' + str(year) + '.ods', engine='odf', sheet_name='Contatti', header=1)
     contact_list['Full Name'] = (contact_list['Cognome'] + ' ' + contact_list['Nome']).str.strip()
-    
-    # Read the sheet of the right month
-    match pd.Timestamp.today().date().month:
-        case 1:
-            appointments_list = pd.read_excel('Appointment_Schedule_'+year+'.ods', engine='odf', sheet_name='Gennaio', header=1)
-        case 2:
-            appointments_list = pd.read_excel('Appointment_Schedule_'+year+'.ods', engine='odf', sheet_name='Febbraio', header=1)
-        case 3:
-            appointments_list = pd.read_excel('Appointment_Schedule_'+year+'.ods', engine='odf', sheet_name='Marzo', header=1)
-        case 4:
-            appointments_list = pd.read_excel('Appointment_Schedule_'+year+'.ods', engine='odf', sheet_name='Aprile', header=1)
-        case 5:
-            appointments_list = pd.read_excel('Appointment_Schedule_'+year+'.ods', engine='odf', sheet_name='Maggio', header=1)
-        case 6:
-            appointments_list = pd.read_excel('Appointment_Schedule_'+year+'.ods', engine='odf', sheet_name='Giugno', header=1)
-        case 7:
-            appointments_list = pd.read_excel('Appointment_Schedule_'+year+'.ods', engine='odf', sheet_name='Luglio', header=1)
-        case 8:
-            appointments_list = pd.read_excel('Appointment_Schedule_'+year+'.ods', engine='odf', sheet_name='Agosto', header=1)
-        case 9:
-            appointments_list = pd.read_excel('Appointment_Schedule_'+year+'.ods', engine='odf', sheet_name='Settembre', header=1)
-        case 10:
-            appointments_list = pd.read_excel('Appointment_Schedule_'+year+'.ods', engine='odf', sheet_name='Ottobre', header=1)
-        case 11:
-            appointments_list = pd.read_excel('Appointment_Schedule_'+year+'.ods', engine='odf', sheet_name='Novembre', header=1)
-        case 12:
-            appointments_list = pd.read_excel('Appointment_Schedule_'+year+'.ods', engine='odf', sheet_name='Dicembre', header=1)
-        case _:
-            appointments_list = pd.read_excel('Appointment_Schedule_'+year+'.ods', engine='odf', sheet_name='Gennaio', header=1)
+
+    #Reading the sheet of the right year and month
+    appointments_list = pd.read_excel('Appointment_Schedule_'+ str(year) +'.ods', engine='odf', sheet_name = month, header=1)
+
     
     # Extract the appointments for the day i (where i goes from 0 to 6 -> today to 7 days after)
-    day_of_today = days_list[(pd.Timestamp.today().date().isoweekday() - 1 + i) % 7] + ' ' + str((pd.Timestamp.today().date() + pd.Timedelta(days=i)).day)
-    print(day_of_today)
-    appointments_day_i = appointments_list[appointments_list['Giorno'] == day_of_today]
+    
+    
+    appointments_day_i = appointments_list[appointments_list['Giorno'] == appointment_day]
 
 
     #################################
-    ##### First slot 09:30-11:00 ####
+    ##### First slot 08:30-10:00 ####
     #################################
-    appointments_day_i_first_slot = appointments_day_i[appointments_day_i['Ora'] == '9:30-11:00']
+    appointments_day_i_first_slot = appointments_day_i[appointments_day_i['Ora'] == time_slots_list[0]]
     names_day_i_first_slot = (
         list(appointments_day_i_first_slot['Marcus']) +
         list(appointments_day_i_first_slot['Jaqueline']) +
@@ -58,18 +48,18 @@ for i in range(7):
         list(appointments_day_i_first_slot['Giuseppe'])
     )
     
-    email_list_first_slot = []
+    phone_numbers_list_first_slot = []
 
     for customer in names_day_i_first_slot:
         for contact in  list(contact_list['Full Name']):
             if customer == contact:
-                email_list_first_slot.append(str(int(contact_list[contact_list['Full Name'] == contact]['Numero Cellulare'].values[0])))
+                phone_numbers_list_first_slot.append(str(int(contact_list[contact_list['Full Name'] == contact]['Numero Cellulare'].values[0])))
 
 
     ###################################
-    ##### Second slot 11:00-12:30 #####
+    ##### Second slot 10:30-12:00 #####
     ###################################
-    appointments_day_i_second_slot = appointments_day_i[appointments_day_i['Ora'] == '11:00-12:30']
+    appointments_day_i_second_slot = appointments_day_i[appointments_day_i['Ora'] == time_slots_list[1]]
     names_day_i_second_slot = (
         list(appointments_day_i_second_slot['Marcus']) +
         list(appointments_day_i_second_slot['Jaqueline']) +
@@ -77,18 +67,18 @@ for i in range(7):
         list(appointments_day_i_second_slot['Giuseppe'])
     )
     
-    email_list_second_slot = []
+    phone_numbers_list_second_slot = []
 
     for customer in names_day_i_second_slot:
         for contact in  list(contact_list['Full Name']):
             if customer == contact:
-                email_list_second_slot.append(str(int(contact_list[contact_list['Full Name'] == contact]['Numero Cellulare'].values[0])))
+                phone_numbers_list_second_slot.append(str(int(contact_list[contact_list['Full Name'] == contact]['Numero Cellulare'].values[0])))
 
 
     ###################################
-    ##### Third slot 14:00-15:30 ######
+    ##### Third slot 12:00-13:30 ######
     ###################################
-    appointments_day_i_third_slot = appointments_day_i[appointments_day_i['Ora'] == '14:00-15:30']
+    appointments_day_i_third_slot = appointments_day_i[appointments_day_i['Ora'] == time_slots_list[2]]
     names_day_i_third_slot = (
         list(appointments_day_i_third_slot['Marcus']) +
         list(appointments_day_i_third_slot['Jaqueline']) +
@@ -96,18 +86,18 @@ for i in range(7):
         list(appointments_day_i_third_slot['Giuseppe'])
     )
     
-    email_list_third_slot = []
+    phone_numbers_list_third_slot = []
 
     for customer in names_day_i_third_slot:
         for contact in  list(contact_list['Full Name']):
             if customer == contact:
-                email_list_third_slot.append(str(int(contact_list[contact_list['Full Name'] == contact]['Numero Cellulare'].values[0])))
+                phone_numbers_list_third_slot.append(str(int(contact_list[contact_list['Full Name'] == contact]['Numero Cellulare'].values[0])))
 
 
     ###################################
-    ##### Fourth slot 15:30-17:00 #####
+    ##### Fourth slot 14:30-16:00 #####
     ###################################
-    appointments_day_i_fourth_slot = appointments_day_i[appointments_day_i['Ora'] == '15:30-17:00']
+    appointments_day_i_fourth_slot = appointments_day_i[appointments_day_i['Ora'] == time_slots_list[3]]
     names_day_i_fourth_slot = (
         list(appointments_day_i_fourth_slot['Marcus']) +
         list(appointments_day_i_fourth_slot['Jaqueline']) +
@@ -115,34 +105,54 @@ for i in range(7):
         list(appointments_day_i_fourth_slot['Giuseppe'])
     )
     
-    email_list_fourth_slot = []
+    phone_numers_list_fourth_slot = []
 
     for customer in names_day_i_fourth_slot:
         for contact in  list(contact_list['Full Name']):
             if customer == contact:
-                email_list_fourth_slot.append(str(int(contact_list[contact_list['Full Name'] == contact]['Numero Cellulare'].values[0])))
+                phone_numers_list_fourth_slot.append(str(int(contact_list[contact_list['Full Name'] == contact]['Numero Cellulare'].values[0])))
 
+
+    ###################################
+    ##### Fifth slot 16:00-17:30 ######
+    ###################################
+    appointments_day_i_fifth_slot = appointments_day_i[appointments_day_i['Ora'] == time_slots_list[4]]
+    names_day_i_fifth_slot = (
+        list(appointments_day_i_fifth_slot['Marcus']) +
+        list(appointments_day_i_fifth_slot['Jaqueline']) +
+        list(appointments_day_i_fifth_slot['Antonio']) +
+        list(appointments_day_i_fifth_slot['Giuseppe'])
+    )
     
-    wait_time = 10
-    wa_message = 'Ciao,\nti ricordiamo il tuo appuntamento del ' + day_of_today + ' alle ore 9:30-11:00. \n A presto! CentroFit'
-    print(email_list_first_slot)
-    for number in email_list_first_slot:
-        kit.sendwhatmsg_instantly('+'+str(number), wa_message, 4, True)
+    phone_numbers_list_fifth_slot = []
+
+    for customer in names_day_i_fifth_slot:
+        for contact in  list(contact_list['Full Name']):
+            if customer == contact:
+                phone_numbers_list_fifth_slot.append(str(int(contact_list[contact_list['Full Name'] == contact]['Numero Cellulare'].values[0])))
 
 
-    wa_message = 'Ciao, ti ricordo il tuo appuntamento di ' + day_of_today + ' alle ore 11:00-12:30'
-    print(email_list_second_slot)
-    for number in email_list_second_slot:
-        kit.sendwhatmsg_instantly('+'+str(number), wa_message, 4, True)
+    ###################################
+    ##### Sixth slot 15:30-17:00 ######
+    ###################################
+    appointments_day_i_sixth_slot = appointments_day_i[appointments_day_i['Ora'] == time_slots_list[5]]
+    names_day_i_sixth_slot = (
+        list(appointments_day_i_sixth_slot['Marcus']) +
+        list(appointments_day_i_sixth_slot['Jaqueline']) +
+        list(appointments_day_i_sixth_slot['Antonio']) +
+        list(appointments_day_i_sixth_slot['Giuseppe'])
+    )
     
+    phone_numbers_list_sixth_slot = []
 
-    wa_message = 'Ciao, ti ricordo il tuo appuntamento di ' + day_of_today + ' alle ore 14:00-15:30'
-    print(email_list_third_slot)
-    for number in email_list_third_slot:
-        kit.sendwhatmsg_instantly('+'+str(number), wa_message, 4, True)
+    for customer in names_day_i_sixth_slot:
+        for contact in  list(contact_list['Full Name']):
+            if customer == contact:
+                phone_numbers_list_sixth_slot.append(str(int(contact_list[contact_list['Full Name'] == contact]['Numero Cellulare'].values[0])))
 
+    print(phone_numbers_list_sixth_slot)
+    
+    phone_numbers_day_i = [phone_numbers_list_first_slot, phone_numbers_list_second_slot, phone_numbers_list_third_slot, phone_numers_list_fourth_slot, phone_numbers_list_fifth_slot, phone_numbers_list_sixth_slot]
 
-    wa_message = 'Ciao, ti ricordo il tuo appuntamento di ' + day_of_today + ' alle ore 15:30-17:00'
-    print(email_list_fourth_slot)
-    for number in email_list_fourth_slot:
-        kit.sendwhatmsg_instantly('+'+str(number), wa_message, 4, True)
+    for i in range(6):
+        send_message(phone_numbers_day_i[i], appointment_day, time_slots_list[i])
