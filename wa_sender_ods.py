9a95ce1716f81c43438d9e1458bfcaba117030c1
Davide <davidefacchinelli2003@gmail.com>
2024-11-19 23:39:48 +0100

    Cleaned the code

--- wa_sender_ods.py
+++ wa_sender_ods.py
@@ -5,7 +5,7 @@ from tkinter import ttk
 from tkinter import messagebox
 
 ###########################
-### FUNCTION DEFINITION ###
+## FUNCTIONS DEFINITION ###
 ###########################
 
 # extract_names_per_slot -> used to extract the names of the customers for a specific time slot
@@ -29,29 +29,36 @@ def extract_phone_numbers_per_slot(names_day_i_slot_i, contact_list):
 def send_message(phone_number_list, appointment_day, appointment_time_slot):
     wa_message = 'Ciao, ti ricordiamo il tuo appuntamento del ' + appointment_day + ' alle ore ' + appointment_time_slot + '. A presto! CentroFit'
     for number in phone_number_list:
-        #kit.sendwhatmsg_instantly('+'+str(number), wa_message, 4, True)
-        print(wa_message)
+        # kit.sendwhatmsg_instantly('+'+str(number), wa_message, 4, True)
+        # print(wa_message)
 
 
-###########################
-######## MAIN PART 1 ######
-###########################
+#####################################
+#### DAYS EXTRACTION FROM NOW ON ####
+#####################################
 
 # Create arrays used in the code to avoid hardcoding
 months_list = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre']
 days_list = ['Lunedì', 'Martedì', 'Mercoledì', 'Giovedì', 'Venerdì', 'Sabato', 'Domenica']
 employee_names = ['Marcus', 'Jaqueline', 'Antonio', 'Giuseppe']
 
-range_of_days = 14
-week_days_from_now_on = []
-for i in range(range_of_days):
-    week_day = days_list[(pd.Timestamp.today().date().isoweekday()+i) % 7] 
-    week_days_from_now_on.append(week_day + ' ' + str((pd.Timestamp.today().date() + pd.Timedelta(days=i+1)).day))
+# Actual extraction of the today and the days from now on
+position_today = days_list.index(days_list[(pd.Timestamp.today().date().isoweekday()) % 7 - 1])
 
-selected_indexes = []
+range_of_days_to_visualize = 14
+week_days_from_now_on = []
+for i in range(range_of_days_to_visualize):
+    week_day = days_list[(pd.Timestamp.today().date().isoweekday()+i) % 7] 
+    month_day = str((pd.Timestamp.today().date() + pd.Timedelta(days=i+1)).day)
+    month = months_list[((pd.Timestamp.today().date() + pd.Timedelta(days=i+1)).month-1)%12]
+    week_days_from_now_on.append(week_day + ' ' + month_day + ' ' + month)
+
+
 ###########################
 ####### GUI LOGIC #########
 ###########################
+selected_indexes = []
+
 def start_gui():
     def on_toggle(index):
         # Toggle the index in the selected list
@@ -69,14 +76,13 @@ def start_gui():
 
     def on_close():
         # Ask the user for confirmation before closing
-        if messagebox.askyesno("Confirm Exit", "Are you sure you want to exit?"):
+        if messagebox.askyesno("Conferma di uscita", "Sicuro di voler uscire?"):
             selected_indexes.clear()  # Clear the selected indexes
             root.destroy()  # Close the window
 
-    
     root = tk.Tk()
-    root.title("Select Days for Appointments")
-    root.geometry("500x400")
+    root.title("Selezione giorni della quale mandare avviso")
+    #root.geometry("500x400")
     root.protocol("WM_DELETE_WINDOW", on_close)
 
     # Create a style for the GUI
@@ -85,7 +91,7 @@ def start_gui():
     style.configure("TButton", font=("Helvetica", 14))
 
     # Add a label at the top
-    title_label = ttk.Label(root, text="Select the Days to Send Notifications", font=("Helvetica", 14, "bold"))
+    title_label = ttk.Label(root, text="Avvisa le persone con appuntamento nei seguenti giorni: ", font=("Helvetica", 14, "bold"))
     title_label.pack(pady=10)
 
     # Create a frame for the day selection grid
@@ -101,7 +107,7 @@ def start_gui():
         toggle_button.grid(row=i % 7, column=i // 7, padx=10, pady=5, sticky='w')  # Arrange in columns of 7
 
     # Add "Send Messages" button at the bottom
-    send_button = ttk.Button(root, text="Send Messages", command=send_message)
+    send_button = ttk.Button(root, text="Invia Messaggi", command=send_message)
     send_button.pack(pady=20)
 
     root.mainloop()
@@ -110,22 +116,24 @@ if __name__ == "__main__":
     start_gui()
 
 
-###########################
-######## MAIN PART 2 ######
-###########################
+#####################################
+### COMPUTATION OF THE WANTED DAY ###
+#####################################
 
 wanted_days = []
-position_today = days_list.index(days_list[(pd.Timestamp.today().date().isoweekday()) % 7 - 1]) #lun=0 dom=6
 
 for i in selected_indexes:
     if i==0:
-        for j in range(1,range_of_days+1):
-            wanted_days.append(((j) - position_today) % range_of_days + 1)
+        for j in range(1,range_of_days_to_visualize+1):
+            wanted_days.append(((j) - position_today) % range_of_days_to_visualize + 1)
     else:
-        print("You asked for: ", i, "and today day position is: ", position_today)
-        wanted_days.append((int(i) - position_today) % range_of_days + 1)
+        wanted_days.append((int(i) - position_today) % range_of_days_to_visualize + 1)
 wanted_days.sort()
-print("ciao", wanted_days)
+
+
+#####################################
+## ITERATION OVER THE WANTED DAYS ###
+#####################################
 
 # Loop over the wanted days to send the messages
 for i in wanted_days:
@@ -135,7 +143,6 @@ for i in wanted_days:
     month_day = (pd.Timestamp.today().date() + pd.Timedelta(days=i)).day
     week_day = days_list[(pd.Timestamp.today().date().isoweekday() - 1 + i) % 7]
     appointment_day = week_day + ' ' + str(month_day)
-    print(appointment_day)
     
     # Read the contact list
     contact_list = pd.read_excel('Appointment_Schedule_' + str(year) + '.ods', engine='odf', sheet_name='Contatti', header=1)
@@ -148,7 +155,6 @@ for i in wanted_days:
     # Extract the appointments for the day i
     appointments_day_i = appointments_list[appointments_list['Giorno'] == appointment_day]
     time_slots_list = list(appointments_day_i['Ora'])
-    print(time_slots_list)
 
     # Extract the phone numbers
     phone_numbers_day_i = []
